7c04073a021687e333875bc6cf91196432ec93a8
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-23 22:27:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,14 @@
 class CashRegister
   attr_accessor :total
+  attr_reader :employee
   
   def initialize()
     @total = 0
+    @employee = discount
+  end
+  
+  def employee
+    
   end
   
   def add_item(title, price)
